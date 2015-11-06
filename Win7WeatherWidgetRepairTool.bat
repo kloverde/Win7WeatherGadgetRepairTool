1b77a47ceabac4874aea58f09ab7227e075c5257
Kurtis LoVerde <kurt@loverde.org>
2015-11-06 00:53:09 -0500

    Initial commit

---
+++ Win7WeatherWidgetRepairTool.bat
@@ -0,0 +1,197 @@
+REM Windows 7 Weather Widget Repair Tool v1.0
+REM Copyright (c) 2015 Kurtis LoVerde
+REM https://www.github.com/kloverde/Win7WeatherWidgetRepairTool
+REM
+REM This script fixes a common problem with the Windows 7 weather
+REM widget where the widget suddenly stops working.  Instead of
+REM showing the current weather conditions, it displays an error
+REM which says "Cannot connect to service."  This script
+REM refreshes the widget's configuration file; the widget will
+REM start working again once the widget is restarted.  This is
+REM usually accomplished by logging out and logging back in.
+REM However, it's possible that by running this script at
+REM startup, the configuration file might be refreshed before the
+REM widget initializes, eliminating the need for logging out.
+REM If you would like to run this script at startup, run the
+REM install.bat script which is packaged with this utility.  It
+REM will create an entry in your registry to run at startup.
+
+REM See LICENSE for this software's licensing terms.
+
+@echo off
+setlocal enabledelayedexpansion
+
+cls
+
+echo Windows 7 Weather Widget Repair Tool v1.0
+echo https://www.github.com/kloverde/Win7WeatherWidgetRepairTool
+
+echo.
+echo.
+
+set RET_FUNCTION_SUCCESS=0
+set RET_FUNCTION_UNINITIALIZED_FAILURE=99
+set RET_ISLICENSEACCEPTED_CONFIG_FILE_DECLINED=-1
+set RET_LICENSEPROMPT_USER_DECLINED=-2
+set RET_FIXWIDGET_FILE_NOT_FOUND=-3
+set RET_FIXWIDGET_COPY_FAIL=-4
+
+set executionDir=%~dp0
+set licenseFullPath=%executionDir%\LICENSE
+
+set scriptConfigDir=%USERPROFILE%\.Win7WeatherWidgetRepairTool
+set scriptConfigFile=config
+set scriptConfigFullPath=%scriptConfigDir%\%scriptConfigFile%
+set scriptConfigFlagAcceptedLicense=acceptedLicense
+
+set widgetConfigDir=%USERPROFILE%\AppData\Local\Microsoft\Windows Live\Services\Cache
+set widgetConfigFile=Config.xml
+set widgetConfigFullPath=%widgetConfigDir%\%widgetConfigFile%
+
+
+REM FUNCTION   : main
+REM PARAMETERS : none
+REM RETURNS    : 0 for successful widget repair; non-zero for unsuccessful repair
+:main
+   setlocal
+
+   REM Default the return values to failure states
+   set retIsLicenseAccepted=%RET_FUNCTION_UNINITIALIZED_FAILURE%
+   set retFixWidget=%RET_FUNCTION_UNINITIALIZED_FAILURE%
+   set exitStatus=%RET_FUNCTION_UNINITIALIZED_FAILURE%
+
+   set strSuccess=Widget repaired successfully.  You might have to log out and log back in again for the change to take effect.
+   set strError=ERROR:  Widget repair failed with return code 
+
+   call :isLicenseAccepted retIsLicenseAccepted
+
+   if !retIsLicenseAccepted!==%RET_FUNCTION_SUCCESS% (
+      call :writeConfigFile
+      call :fixWidget retFixWidget
+
+      if not !retFixWidget!==%RET_FUNCTION_SUCCESS% (
+         set strError=!strError!!retFixWidget!
+         set exitStatus=!retFixWidget!
+      ) else (
+         set exitStatus=%RET_FUNCTION_SUCCESS%
+      )
+   ) else (
+      set strError=!strError!!retIsLicenseAccepted!
+      set exitStatus=!retIsLicenseAccepted!
+   )
+
+   if not !exitStatus!==%RET_FUNCTION_SUCCESS% (
+      if not !retIsLicenseAccepted!==%RET_LICENSEPROMPT_USER_DECLINED% (         
+         echo.
+         echo !strError!
+      )
+   ) else (
+      echo.
+      echo %strSuccess%
+   )
+
+   exit /b !exitStatus!
+
+   endlocal
+   goto :eof
+
+REM FUNCTION   : isLicenseAccepted
+REM PARAMETERS : reference variable for return value
+REM RETURNS    : All possible failure codes from licensePrompt();
+REM              RET_ISLICENSEACCEPTED_CONFIG_FILE_DECLINED if the config file indicates the terms were not accepted;
+REM              RET_FUNCTION_SUCCESS if license is accepted
+:isLicenseAccepted
+   setlocal
+      set found=0
+      set retLicensePrompt=%RET_LICENSEPROMPT_USER_DECLINED%
+   endlocal
+
+   if not exist "%scriptConfigFullPath%" (
+      echo LICENSING TERMS
+      echo.
+      type %licenseFullPath%
+      echo.
+
+      call :licensePrompt retLicensePrompt
+      set %~1=!retLicensePrompt!
+   ) else (
+      REM Don't use findstr.  It has a silly bug which requires the file to have a newline
+      REM at the end, which could mess up parsing if the user manually edits the file.
+      REM findstr /x "%scriptConfigFlagAcceptedLicense%" "%scriptConfigFullPath%"
+
+      for /f %%s in ( %scriptConfigFullPath% ) do (
+         if %scriptConfigFlagAcceptedLicense%==%%s (
+            set found=1
+         )
+      )
+
+      if !found!==1 (
+         set %~1=%RET_FUNCTION_SUCCESS%
+      ) else (
+         set %~1=%RET_ISLICENSEACCEPTED_CONFIG_FILE_DECLINED%
+      )
+   )
+
+   goto :eof
+
+REM FUNCTION   : licensePrompt
+REM PARAMETERS : reference variable for return value
+REM RETURNS    : RET_LICENSEPROMPT_USER_DECLINED if user did not accept the terms;
+REM              RET_FUNCTION_SUCCESS if the user accepted the terms
+:licensePrompt
+   set /p accept=Do you accept the licensing terms^? ^(Y/N^) 
+
+   if /i "!accept!"=="Y" (
+      set %~1=%RET_FUNCTION_SUCCESS%
+      goto :eof
+   )
+      
+   if /i "!accept!"=="N" (
+      set %~1=%RET_LICENSEPROMPT_USER_DECLINED%
+      goto :eof
+   )
+
+   goto licensePrompt
+
+REM FUNCTION   : fixWidget
+REM PARAMETERS : reference variable for return value
+REM RETURNS    : RET_FIXWIDGET_FILE_NOT_FOUND if the widget config file could not be found
+REM              RET_FIXWIDGET_COPY_FAIL if file unsuccessfully written;
+REM              RET_FUNCTION_SUCCESS if file successfully written
+:fixWidget
+   if not exist "%widgetConfigFullPath%" (
+      set %~1=%RET_FIXWIDGET_FILE_NOT_FOUND%
+   ) else (
+      echo Repairing widget...
+
+      pushd %widgetConfigDir%
+      copy %widgetConfigFile% /b+ ,,/y
+
+      if !errorlevel!==0 (
+         set %~1=%RET_FUNCTION_SUCCESS%
+      ) else (
+         set %~1=%RET_FIXWIDGET_COPY_FAIL%
+      )
+
+      popd
+   )
+
+   goto :eof
+
+REM FUNCTION   : writeConfigFile
+REM PARAMETERS : none
+REM RETURNS    : nothing
+:writeConfigFile
+   if not exist "%scriptConfigDir%" (
+      mkdir "%scriptConfigDir%"
+   )
+
+   REM Don't put a space in front of the redirect operator!  The space will be written to the file,
+   REM which will mess up parsing.
+
+   echo %scriptConfigFlagAcceptedLicense%> %scriptConfigFullPath%
+   goto :eof
+
+
+REM closing endlocal from the beginning of the script
+endlocal
